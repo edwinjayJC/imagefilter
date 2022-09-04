aa02e49968b766ed3256bd2013df3304486a86a7
Edwin Jay <edwinj@mit.edu>
2022-09-04 08:15:02 +0200

    Updated implementation to indicate correct types of Express in server.ts, updated package.json

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,36 @@
-import express from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
 
-  const app = express();
+  const app: Express = express();
 
   const port = process.env.PORT || 8082;
   
   app.use(bodyParser.json());
 
-  app.use(function(req, res, next) {
+  app.use(function(req: Request, res: Response, next: NextFunction) {
     res.header("Access-Control-Allow-Origin", "http://localhost:8100");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
 
-  app.get("/filteredimage", async ( req : any, res : any ) => {
+  app.get("/filteredimage", async ( req: Request, res: Response ) => {
     let imageUrl = req.query.image_url;
 
     if (!imageUrl) {
       return res.status(400).send("Image URL missing or invalid");
     }
 
-    const imagePath = await filterImageFromURL(imageUrl);
+    const imagePath: string = await filterImageFromURL(imageUrl);
 
     res.sendFile(imagePath, function(){
       deleteLocalFiles([imagePath]);
     });
   });
   
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
